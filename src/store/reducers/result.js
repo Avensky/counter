b027bc889fd87dc508ead923e4961225b55833b2
uriel zacarias <urielzacarias@gmail.com>
2020-03-22 11:18:42 -0700

    made changes to make code leaner

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -1,22 +1,18 @@
 import * as actionTypes from '../actions/actionsTypes';
+import { updateObject } from '../utility';
 
 const initialState = {
     results: []
 }
+const deleteResult = (state, action) => {
+    const newArray = state.results.filter(result => result.id !== action.resElId);
+    return updateObject(state, {results: newArray})
+}
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case actionTypes.STORE_RESULT:
-            return {
-                ...state,
-                results: state.results.concat({id: new Date(), value: action.result })
-            }
-        case actionTypes.DELETE_RESULT:
-            const newArray = state.results.filter(result => result.id !== action.resElId);
-            return {
-                ...state,
-                results: newArray
-            }
+        case actionTypes.STORE_RESULT: return updateObject(state,{results: state.results.concat({id: new Date(), value: action.result })})
+        case actionTypes.DELETE_RESULT: return deleteResult(state, action)
     }
     return state;
 }
